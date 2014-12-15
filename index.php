a1748b4c022080510f82add32370acf87b881207
Andrey Granat <granat.andrey@gmail.com>
2014-12-15 19:08:10 +0200

    separation parts of the site

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/head.html'; ?>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $("#carousel").featureCarousel({
+			autoPlay:7000,
+			trackerIndividual:false,
+			trackerSummation:false,
+			topPadding:50,
+			smallFeatureWidth:.9,
+			smallFeatureHeight:.9,
+			sidePadding:0,
+			smallFeatureOffset:0
+		});
+      });
+    </script>
+	<!--[if lt IE 7]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        	<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+    </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+        <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+	<![endif]-->
+</head>
+<body id="page1">
+	<div class="extra">
+    	<!--==============================header=================================-->
+        <header>
+        	<?php include 'includes/header.html'; ?>
+            <div class="row-bot">
+            	<div class="center-shadow">
+                	<div class="carousel-container">
+                      <div id="carousel">
+                        <div class="carousel-feature">
+                          <a href="#"><img class="carousel-image" alt="" src="images/gallery-img1.png"></a>                          
+                        </div>
+                        <div class="carousel-feature">
+                          <a href="#"><img class="carousel-image" alt="" src="images/gallery-img3.png"></a>
+                        </div>
+                        <div class="carousel-feature">
+                          <a href="#"><img class="carousel-image" alt="" src="images/gallery-img2.png"></a>
+                        </div>
+                      </div>
+    				</div>
+                </div>
+            </div>
+        </header>
+        
+        <!--==============================content================================-->
+        <section id="content"><div class="ic">More Website Templates @ TemplateMonster.com. December10, 2011!</div>
+            <div class="main">
+                <div class="container_12">
+                    <div class="wrapper">
+                        <article class="grid_8">
+                        	<h3>Welcome! Why Choose Us?</h3>
+                            <em class="text-1 margin-bot">Consulting is one of <a class="link" target="_blank" href="http://blog.templatemonster.com/free-website-templates/">free website templates</a> created by TemplateMonster.com team. This website template is optimized for 1280X1024 screen resolution. It is also XHTML &amp; CSS valid.</em>
+                            <div class="wrapper p4">
+                            	<article class="grid_4 alpha">
+                                	<div class="wrapper p1">
+                                    	<figure class="img-indent2"><img src="images/page1-img1.png" alt="" /></figure>
+                                        <div class="extra-wrap">
+                                        	<div class="indent-top">
+                                            	<h4>STARTING UP<em>How-to</em></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="prev-indent-bot">This <a class="color-2" href="http://blog.templatemonster.com/2011/12/10/free-website-template-jquery-slider-consulting-business/" target="_blank">Consulting Template</a> goes with two<br> packages – with PSD source files and<br> without them.</p>
+                                    <a class="link-1" href="#">Know More</a>
+                                </article>
+                                <article class="grid_4 omega">
+                                	<div class="wrapper p1">
+                                    	<figure class="img-indent2"><img src="images/page1-img2.png" alt="" /></figure>
+                                        <div class="extra-wrap">
+                                        	<div class="indent-top">
+                                            	<h4>Development<em>Strategies</em></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="prev-indent-bot">PSD source files of this Consulting Template<br> are available for free for the registered<br> members of TemplateMonster.com.</p>
+                                    <a class="link-1" href="#">Know More</a>
+                                </article>
+                            </div>
+                            <div class="wrapper">
+                            	<article class="grid_4 alpha">
+                                	<div class="wrapper p1">
+                                    	<figure class="img-indent2"><img src="images/page1-img3.png" alt="" /></figure>
+                                        <div class="extra-wrap">
+                                        	<div class="indent-top">
+                                            	<h4>Planning<em>Need Help?</em></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="prev-indent-bot">The basic package of this Consulting<br> Template (without PSD source) is available<br> for anyone without registration.</p>
+                                    <a class="link-1" href="#">Know More</a>
+                                </article>
+                                <article class="grid_4 omega">
+                                	<div class="wrapper p1">
+                                    	<figure class="img-indent2"><img src="images/page1-img4.png" alt="" /></figure>
+                                        <div class="extra-wrap">
+                                        	<div class="indent-top">
+                                            	<h4>INTEGRATION<em>Exploit your ideas</em></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="prev-indent-bot">This template has several pages: <a class="color-2" href="index.html">Home</a>,<br>  <a class="color-2" href="company.html">Company</a>, <a class="color-2" href="services.html">Services</a>, <a class="color-2" href="projects.html">Projects</a>, <a class="color-2" href="contact.html">Contacts</a> (note<br> that contact us form – doesn’t work).</p>
+                                    <a class="link-1" href="#">Know More</a>
+                                </article>
+                            </div>
+                        </article>
+                        <article class="grid_4">
+                        	<h3>Testimonials</h3>
+                            <div class="wrapper p3">
+                            	<figure class="img-indent"><a href="#"><img class="img-border" src="images/page1-img5.jpg" alt="" /></a></figure>
+                                <div class="extra-wrap">
+                                	<span class="text-2">Director</span>
+                                    <h4 class="p2">Mary Ryan</h4>
+                                    <p class="prev-indent-bot">Ut vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum atque corrupti...</p>
+                                    <a class="link-1" href="#">Read More</a>
+                                </div>
+                            </div>
+                            <div class="wrapper p3">
+                            	<figure class="img-indent"><a href="#"><img class="img-border" src="images/page1-img6.jpg" alt="" /></a></figure>
+                                <div class="extra-wrap">
+                                	<span class="text-2">Senior assistant</span>
+                                    <h4 class="p1">Bill Joel</h4>
+                                    <p class="prev-indent-bot">Ut vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum atque corrupti...</p>
+                                    <a class="link-1" href="#">Read More</a>
+                                </div>
+                            </div>
+                            <div class="wrapper">
+                            	<figure class="img-indent"><a href="#"><img class="img-border" src="images/page1-img7.jpg" alt="" /></a></figure>
+                                <div class="extra-wrap">
+                                	<span class="text-2">Junior researcher</span>
+                                    <h4 class="prev-indent-bot">Michael Anderson</h4>
+                                    <p class="prev-indent-bot">Ut vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum atque corrupti...</p>
+                                    <a class="link-1" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="block"></div>
+        </section>
+    </div>
+	
+	<!--==============================footer=================================-->
+    <footer>
+        <?php include 'includes/footer.html'; ?>
+    </footer>
+	<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
