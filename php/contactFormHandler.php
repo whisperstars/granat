084879ea7eb9a0217052f795ffbec9028e5c0b20
Andrey Granat <granat.andrey@gmail.com>
2015-04-23 17:20:20 +0300

    contact form updated: save form to file

--- php/contactFormHandler.php
+++ php/contactFormHandler.php
@@ -1,8 +1,25 @@
 <?php
+
     $mail_body = '';
-    $mail_body .= "Имя" . $_POST["name"] . "\n";
-    $mail_body .= "Контакт" . $_POST["contact"] . "\n";
-    $mail_body .= "Сообщение" . $_POST["message"];
+    $mail_body .= "Имя:       " . $_POST["name"] . "\n";
+    $mail_body .= "Контакт:   " . $_POST["contact"] . "\n";
+    $mail_body .= "Сообщение: " . $_POST["message"];
+    
+    $filename = "../data/siteMessage.txt";
+
+    if (is_writable($filename)) {
+        if (!$handle = fopen($filename, 'a')) {
+            echo "Не могу открыть файл ($filename)";
+            exit;
+        }
+
+        if (fwrite($handle, $mail_body . "\n \n [=========================================================================] \n \n") === FALSE) {
+            echo "Не могу произвести запись в файл ($filename)";
+            exit;
+        }
+    }
+
+    
 
     mail("info@granat.dp.ua", "Сообщение с сайта granat.dp.ua", $mail_body);
     mail("iam1992kreol@ya.ru", "Сообщение с сайта granat.dp.ua", $mail_body);
